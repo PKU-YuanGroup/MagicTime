a772e8e6c0084778b77772db94034ea0a51cb913
SHYuanBest <140951558+SHYuanBest@users.noreply.github.com>
2024-04-10 12:35:40 +0800

    Add files via upload

---
+++ prepare_weights/down_base_model.sh
@@ -0,0 +1,3 @@
+git lfs install
+git clone https://huggingface.co/runwayml/stable-diffusion-v1-5 ckpts/Base_Model
+wget https://huggingface.co/guoyww/animatediff/resolve/main/v3_sd15_mm.ckpt -O ckpts/Base_Model/motion_module/motion_module.ckpt
---
+++ prepare_weights/down_magictime_module.sh
@@ -0,0 +1,4 @@
+git lfs install
+git clone https://huggingface.co/BestWishYsh/MagicTime
+mv MagicTime/Magic_Weights/ ckpts/
+rm -r MagicTime
